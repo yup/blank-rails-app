41e36e33a7c140e4d58135839212059935bd0f27
August Lilleaas <augustlilleaas@gmail.com>
2009-03-09 22:31:16 +0100

    Added that rescue_from I always use.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
   filter_parameter_logging :password
+  
+  
+  rescue_from ActiveRecord::RecordInvalid, :with => :handle_invalid_record
+  
+  private
+  
+  # Call .create! and .update_attributes!, which will raise an exception and
+  # render either new or edit, depending on the state of the record, if the
+  # validation failed.
+  def handle_invalid_record(exception)
+    render :action => (exception.record.new_record? ? "new" : "edit" )
+  end
 end
